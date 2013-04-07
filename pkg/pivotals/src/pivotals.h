1d3087e04cff56cecdeff2c1a7716be89e2423bd
silkwodj <silkwodj@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-07 22:06:21 +0000

    Added Lognormal MRR function X on Y.  Re-named single MRR.cpp file.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/abernethy@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/pivotals/src/pivotals.h
+++ pkg/pivotals/src/pivotals.h
@@ -8,7 +8,7 @@ RcppExport SEXP pivotalMCw2p(SEXP arg1, SEXP arg2, SEXP arg3, SEXP arg4, SEXP ar
 //RcppExport SEXP wbtk_pivotalMCw2(SEXP arg1, SEXP arg2, SEXP arg3, SEXP arg4, SEXP arg5, SEXP arg6);
 RcppExport SEXP medianRank (SEXP arg1);
 RcppExport SEXP MRRw2pXonY (SEXP arg1, SEXP arg2);
-
+RcppExport SEXP MRRln2pXonY (SEXP arg1, SEXP arg2);
 
 
 #endif
