c104b2c012f86be4daa5d22017db578834ada15c
jurgensymynck <jurgensymynck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-05-30 07:39:33 +0000

    Added some files that were missing for building this package on r-forge. Also modified NAMESPACE file; removed some entries
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/abernethy@24 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/abrem/R/F0inv.R
@@ -0,0 +1,52 @@
+# R package 'abrem'
+# Abernethy Reliability Methods
+# Implementations of lifetime data analysis methods described in
+# 'The New Weibull Handbook, Fifth edition' by Dr. Robert B. Abernethy.
+# May 2013, Jurgen Symynck
+# Copyright 2013, Jurgen Symynck
+#
+# For the latest version of this file, check the Subversion repository at
+# http://r-forge.r-project.org/projects/abernethy/
+#
+# Disclaimer:
+#    The author is not affiliated with Dr. Abernethy or Wes Fulton - CEO of
+#    Fulton Findings(TM) and author of the software package SuperSMITH
+#-------------------------------------------------------------------------------
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    For more info on this software and its predecesser, the "weibulltoolkit",
+#    consult following documents:
+#
+#    - "Weibull analysis using R, in a nutshell",
+#      (Jurgen Symynck, Filip De Bal, 2010)
+#    - "Monte Carlo pivotal confidence bounds for Weibull analysis
+#      with implementations in R",
+#      (Jurgen Symynck, Filip De Bal, 2011)
+#
+# +-----------------------------------+
+# |  execute this program with R:     |
+# |  http://www.r-project.org/        |
+# +-----------------------------------+
+#
+F0inv <- function(p){
+   # transformation function to plot its argument
+   # on the y-axis of the Weibull plot. This transformation function
+   # lets the Weibull curve appear as a straight line on the weibull paper
+   #
+   # This is also the inverse Cumulative Distribution function of the
+   # standardized Weibull plot with beta=eta=1
+   # comparing  both implementationss of F0inv() with
+   # system.time() does not show any significant difference
+   #   log(log(1/(1-p)))}
+   log(qweibull(p,1,1))}
---
+++ pkg/abrem/R/mrr.R
@@ -0,0 +1,54 @@
+# R package 'abrem'
+# Abernethy Reliability Methods
+# Implementations of lifetime data analysis methods described in
+# 'The New Weibull Handbook, Fifth edition' by Dr. Robert B. Abernethy.
+# May 2013, Jurgen Symynck
+# Copyright 2013, Jurgen Symynck
+#
+# For the latest version of this file, check the Subversion repository at
+# http://r-forge.r-project.org/projects/abernethy/
+#
+# Disclaimer:
+#    The author is not affiliated with Dr. Abernethy or Wes Fulton - CEO of
+#    Fulton Findings(TM) and author of the software package SuperSMITH
+#-------------------------------------------------------------------------------
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    For more info on this software and its predecesser, the "weibulltoolkit",
+#    consult following documents:
+#
+#    - "Weibull analysis using R, in a nutshell",
+#      (Jurgen Symynck, Filip De Bal, 2010)
+#    - "Monte Carlo pivotal confidence bounds for Weibull analysis
+#      with implementations in R",
+#      (Jurgen Symynck, Filip De Bal, 2011)
+#
+# +-----------------------------------+
+# |  execute this program with R:     |
+# |  http://www.r-project.org/        |
+# +-----------------------------------+
+#
+mrr <- function(d=NULL,is.XonY.regression=TRUE){
+   if(!is.data.frame(d) || is.null(d$mrank) || is.null(d$time))
+      stop("Missing 'time', 'mrank' column or argument is no data frame. Is the data median ranked?")
+   if(is.XonY.regression){
+      # options()$na.action should be set to "na.omit" to deal
+      # properly with censored items in the dataframe
+      fwb <- lm(log(d$time) ~ F0inv(d$mrank),d)
+   }else{
+      # y on x regression. Do not use this , use x on y regression.
+      # possibly BUGGY!
+      fwb <- lm(F0inv(d$mrank) ~ log(d$time),d)}
+   fwb}
+   
