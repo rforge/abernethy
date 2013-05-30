fb38b5138e9afae0d7e149f1d37b5209c994b094
jurgensymynck <jurgensymynck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-05-30 16:43:44 +0000

    added seq.log and seq.wb
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/abernethy@25 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/abrem/R/seq.log.R
@@ -0,0 +1,45 @@
+# R package 'abrem'
+# Abernethy Reliability Methods
+# Implementations of lifetime data analysis methods described in
+# 'The New Weibull Handbook, Fifth edition' by Dr. Robert B. Abernethy.
+# May 2013, Jurgen Symynck
+# Copyright 2013, Jurgen Symynck
+#
+# For the latest version of this file, check the Subversion repository at
+# http://r-forge.r-project.org/projects/abernethy/
+#
+# Disclaimer:
+#    The author is not affiliated with Dr. Abernethy or Wes Fulton - CEO of
+#    Fulton Findings(TM) and author of the software package SuperSMITH
+#-------------------------------------------------------------------------------
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    For more info on this software and its predecesser, the "weibulltoolkit",
+#    consult following documents:
+#
+#    - "Weibull analysis using R, in a nutshell",
+#      (Jurgen Symynck, Filip De Bal, 2010)
+#    - "Monte Carlo pivotal confidence bounds for Weibull analysis
+#      with implementations in R",
+#      (Jurgen Symynck, Filip De Bal, 2011)
+#
+# +-----------------------------------+
+# |  execute this program with R:     |
+# |  http://www.r-project.org/        |
+# +-----------------------------------+
+#
+seq.log <- function(from,to,base=c(1,2,5)){
+   r <- NULL
+   for(i in floor(log10(from)):floor(log10(to)))r <- c(r,base*10^i)
+   r[r >= from & r<=to]}
