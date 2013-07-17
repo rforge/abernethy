dc8a87564c4683e4fa78875a18ae8239964cefc9
silkwodj <silkwodj@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-07-17 20:06:00 +0000

    subtle change in using namespace declaration
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/abernethy@40 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/debias/src/debias.h
+++ pkg/debias/src/debias.h
@@ -3,6 +3,7 @@
 
 
 #include <RcppArmadillo.h>
+using namespace Rcpp ;
 
 RcppExport SEXP MLEw2p (SEXP arg1, SEXP arg2, SEXP arg3);
 RcppExport SEXP MLEln2p (SEXP arg1, SEXP arg2, SEXP arg3, SEXP arg4);
