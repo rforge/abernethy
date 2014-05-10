9135001709b0f475fb4ae52ec03eabc91a9d6663
jtormerod <jtormerod@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-10 22:26:42 +0000

    added Abernethy's P-value and CCC2 determinations from correlation with S=10^8 pivotal sampling.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/abernethy@88 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/abremPivotals/src/abremPivotals.h
+++ pkg/abremPivotals/src/abremPivotals.h
@@ -1,8 +1,13 @@
 #ifndef _abremPivotals_H
 #define _abremPivotals_H
 
-#include <RcppArmadillo.h>
+struct AbPval{
+	double Pval,CCC2;
+};
 
+#ifdef __cplusplus
+
+#include <RcppArmadillo.h>
 
 //RcppExport SEXP pivotalMCw2p(SEXP arg1, SEXP arg2, SEXP arg3, SEXP arg4, SEXP arg5, SEXP arg6);
 //RcppExport SEXP pivotalMCln2p(SEXP arg1, SEXP arg2, SEXP arg3, SEXP arg4, SEXP arg5, SEXP arg6);
@@ -15,6 +20,12 @@ RcppExport SEXP LSLRg2pXonY (SEXP arg1, SEXP arg2);
 RcppExport SEXP LSLRg2pYonX (SEXP arg1, SEXP arg2);
 
 RcppExport SEXP LSLR(SEXP arg1, SEXP arg2, SEXP arg3, SEXP arg4);
+RcppExport SEXP CallgetCCC2(SEXP arg1, SEXP model);
+RcppExport SEXP CallgetPvalue(SEXP arg1, SEXP arg2, SEXP arg3);
+
+extern "C" double getCCC2( int F, int model);
+extern "C" struct AbPval getPvalue(int F, double R2, int model);
 
 
+#endif
 #endif
