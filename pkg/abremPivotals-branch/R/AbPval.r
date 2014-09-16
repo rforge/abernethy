e8c70ccd4ebe396ee8aa2d6a78eda6d0d5df7c4e
jtormerod <jtormerod@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-09-16 01:49:31 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/abernethy@107 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/abremPivotals-branch/R/AbPval.r
+++
@@ -1,7 +0,0 @@
-AbPval<-function(
-F,R2,model="weibull")  {
-m<-1
-if(model=="lnorm") {m<-0}
-
-	.Call( "CallgetPvalue", F, R2, m, PACKAGE = "abremPivotals" )
-}
--- pkg/abremPivotals-branch/R/getCCC2.r
+++
@@ -1,6 +0,0 @@
-getCCC2<-function(F, model="weibull")  {
-m<-1
-if(model=="lnorm") {m<-0}
-
-	.Call( "CallgetCCC2", F, m, PACKAGE = "abremPivotals" )
-}
